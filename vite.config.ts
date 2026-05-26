c4a92a76654fc5d13bb2336d52a384ecac6d5565
meamarp <appotdar05@gmail.com>
2026-05-27 01:54:08 +0530

    fix: set base path for deployment in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/totaltensorlabs.github.io/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
